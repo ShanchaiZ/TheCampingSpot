432c0e552806126531b53b25dcdc342fcbe1ec5b
ShanchaiZ <shanchai.zahid@gmail.com>
2023-01-09 10:41:16 -0500

    required express, set Port number 3001

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+
+// APP IS LISTENING ON PORT:
+//=============================================================================================
+app.listen(3001, () => {
+    console.log("APP IS LISTENING ON PORT 3001!")
+})
