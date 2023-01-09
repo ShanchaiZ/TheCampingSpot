4b66f51014713feea77bd24493985389b2ee76d5
ShanchaiZ <shanchai.zahid@gmail.com>
2023-01-09 12:21:48 -0500

    established connection mongodb server to mongoose with a try/catch statement for error - Works

--- app.js
+++ app.js
@@ -1,16 +1,39 @@
 //Installed Dependencies:
+//-------------------------------------------------------------------------------------------------------------------------------------------------
 const express = require("express");
-const app = express();
 const path = require("path");
+const mongoose = require("mongoose");
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+//Importing Mongoose:
+mongoose.connect("mongodb://127.0.0.1:27017/TheCampingSpot", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    //Process of Connecting mongodb server to mongoose: 
+    //"localhost" was replaced with "127.0.0.1" and /TheCampingSpot is the name of our new created database.
+    .then(() => {
+        console.log("MongoDB Server Connection is Open!");
+    })
+    .catch(err => {
+        console.log("There is Mongo Connection Error!");
+        console.log(err);
+    })
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+
+const app = express();
+
 
 
 
 //Middlewares:
+//-------------------------------------------------------------------------------------------------------------------------------------------------
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 
 //Routes:
+//-------------------------------------------------------------------------------------------------------------------------------------------------
 app.get("/", (req, res) => {
     res.render("home");
 })
