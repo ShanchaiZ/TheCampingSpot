dc11d5370a3ecf532b7c7ac16ccb65d3936e606d
ShanchaiZ <shanchai.zahid@gmail.com>
2023-01-09 10:48:32 -0500

    verifying correct set up of express server by using a simple get request - Works

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 
 
+app.get("/", (req, res) => {
+    res.send("Hello Campers!!");
+})
+
 // APP IS LISTENING ON PORT:
 //=============================================================================================
 app.listen(3001, () => {
