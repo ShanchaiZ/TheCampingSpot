3a90c463a9a66f0c2b2f01b08b33dc7838f00160
ShanchaiZ <shanchai.zahid@gmail.com>
2023-01-10 11:42:14 -0500

    added logic for seeding the database  with cities and states from the seeds array. Used Async/await function and 'for loop' to loop the seeding of cities x50. Random campground location selected from cities seed array by stringing randomized city1000.city and city1000.state then saving to db. git status git status git add .

--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,9 @@
 //This file will be independent/self-contained from the node anytime we want to seed the database connect to Mongoose and use the Model to make basic data.
 //-------------------------------------------------------------------------------------------------------------------------------------------------
 const mongoose = require("mongoose");
+const cities = require("./cities");
 const Campground = require("../models/campground");
+const campground = require("../models/campground");
 //-------------------------------------------------------------------------------------------------------------------------------------------------
 //Importing Mongoose:
 mongoose.connect("mongodb://127.0.0.1:27017/TheCampingSpot", {
@@ -20,10 +22,18 @@ mongoose.connect("mongodb://127.0.0.1:27017/TheCampingSpot", {
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------
 
-//seeding database logic and testing connection to mongoDB:
+//LOGIC: Seeding the database  with cities and states from the seeds array:
 const seedDB = async () => {
     await Campground.deleteMany({});
-    const c = new Campground({ title: "Amazon River" });
-    await c.save();
+    //looping the seed data 50 times and random 1000 because there are 1000 seeded cities in the cities seed array.
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const camp = new Campground({
+            //location = (random city from the 1000 cities array and .city is the property we want.) + (random State from 1000 cities array and the random State property)
+            location: `${cities[random1000].city}, ${cities[random1000].state}`
+        });
+        await camp.save();
+    }
 }
 seedDB();
+//-------------------------------------------------------------------------------------------------------------------------------------------------
