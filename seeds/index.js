1bfc3fab29b1b8fa14a0407fcf059b45520f1ef4
ShanchaiZ <shanchai.zahid@gmail.com>
2023-01-10 12:11:17 -0500

    deleted duplicate variable

--- seeds/index.js
+++ seeds/index.js
@@ -3,7 +3,6 @@
 const mongoose = require("mongoose");
 const cities = require("./cities");
 const Campground = require("../models/campground");
-const campground = require("../models/campground");
 //-------------------------------------------------------------------------------------------------------------------------------------------------
 //Importing Mongoose:
 mongoose.connect("mongodb://127.0.0.1:27017/TheCampingSpot", {
