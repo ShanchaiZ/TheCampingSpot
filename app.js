e81065f6143c16e4b9a1fdd7e2510fe064d8a9b1
ShanchaiZ <shanchai.zahid@gmail.com>
2023-01-12 12:14:02 -0500

    added a new POST route in the views template with basic form that has title,location and a button

--- app.js
+++ app.js
@@ -35,20 +35,33 @@ app.set("views", path.join(__dirname, "views"));
 //-------------------------------------------------------------------------------------------------------------------------------------------------
 app.get("/", (req, res) => {
     res.render("home");
-})
+});
 
 
 //Index Route: lists all the campgrounds available:
 app.get("/campgrounds", async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render("campgrounds/index", { campgrounds });
-})
+});
+
+//Get Route: Form Creation
+app.get("/campgrounds/new", (req, res) => {
+    res.render("campgrounds/new");
+});
 
 //Show Route: details of all campgrounds:
 app.get("/campgrounds/:id", async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render("campgrounds/show", { campground });
-})
+});
+
+
+
+
+
+
+//Post Route: Create a Campground
+
 
 // //testing: Creation of a new campground using a route:
 // app.get("/makecampground", async (req, res) => {
