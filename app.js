6a94a3054e8cc1c2cbf86d9eeaedf71e8295258f
ShanchaiZ <shanchai.zahid@gmail.com>
2023-01-17 13:33:58 -0500

    npm installed and required ejs-mate to make boilerplate templates of all views, create a boilerplate html with ejs to make dynamic templates in the layout directory.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate")
 const methodOverride = require("method-override");
 const Campground = require("./models/campground");
 
@@ -27,6 +28,7 @@ const app = express();
 
 //Middlewares:
 //-------------------------------------------------------------------------------------------------------------------------------------------------
+app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
