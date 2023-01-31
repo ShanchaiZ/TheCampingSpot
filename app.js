b00112206ce8448ed81fb7d477718a161107171e
ShanchaiZ <shanchai.zahid@gmail.com>
2023-01-30 23:11:02 -0500

    To render User reviews associated with its campground: applied method.populate(and refer the data from the review section of the model). Displayed rating and reviews on the associated campground show page template

--- app.js
+++ app.js
@@ -102,7 +102,7 @@ app.post("/campgrounds", validateCampground, catchAsync(async (req, res, next) =
 
 //SHOW ROUTE: details of all campgrounds:
 app.get("/campgrounds/:id", catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
+    const campground = await Campground.findById(req.params.id).populate("reviews");
     res.render("campgrounds/show", { campground });
 }));
 
