0ade4996328f8f2cfbaa8a57d6c94d3ce6dce4dc
ShanchaiZ <shanchai.zahid@gmail.com>
2023-02-11 17:46:31 -0500

    Added Registration route logic that creates a user using username, email and hashed password. note: since there is no tracking session = this is not a login authentication.

--- routes/users.js
+++ routes/users.js
@@ -6,5 +6,13 @@ router.get("/register", (req, res) => {
     res.render("./users/register");
 })
 
+router.post("/register", async (req, res) => {
+    const { username, email, password } = req.body;
+    const user = new User({ email, username });
+    const registeredUser = await User.register(user, password);
+    console.log(registeredUser);
+    req.flash("success", "Welcome to TheCampingSpot");
+    res.redirect("/campgrounds");
+})
 
 module.exports = router;
