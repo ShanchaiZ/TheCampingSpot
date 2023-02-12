81389a9efc56f82fa9cd0862667ed35295a3c9e7
ShanchaiZ <shanchai.zahid@gmail.com>
2023-02-12 02:08:33 -0500

    Fixed user register route so that once a user has registered, they are THEN automatically logged in. This increases user experience as there is no double login of registering AND logging in. Note: req.login (user, err callback) is a passport method helper needs a callback to function (therefore err handler call back is passed) and it does not support await!

--- routes/users.js
+++ routes/users.js
@@ -15,13 +15,16 @@ router.get("/register", (req, res) => {
 })
 
 //POST ROUTE: creating a user on submission of registration form
-router.post("/register", catchAsync(async (req, res) => {
+router.post("/register", catchAsync(async (req, res, next) => {
     try {
         const { username, email, password } = req.body;
         const user = new User({ email, username });
         const registeredUser = await User.register(user, password);
-        req.flash("success", "Welcome to TheCampingSpot");
-        res.redirect("/campgrounds");
+        req.login(registeredUser, err => { //passport helper that make user registration count as a login!
+            if (err) return next(err);
+            req.flash("success", "Welcome to TheCampingSpot");
+            res.redirect("/campgrounds");
+        });
     } catch (e) {
         req.flash("error", e.message);
         res.redirect("register");
