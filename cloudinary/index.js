f3010d13a71afcc0d9f030408b262d76f2518f0a
ShanchaiZ <shanchai.zahid@gmail.com>
2023-02-21 17:02:55 -0500

    completed set up of cloudinary configuration and cloud storage

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -1,2 +1,24 @@
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
+
+
+//Set up cloudinary configuration for storage:
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET
+});
+
+
+//Set up the Storage Specifications in the cloudinary account:
+const storage = new CloudinaryStorage({
+    cloudinary,
+    folder: "TheCampSpot",
+    allowedFormats: ["jpeg", "png", "jpg",]
+});
+
+
+module.exports = {
+    cloudinary,
+    storage
+}
