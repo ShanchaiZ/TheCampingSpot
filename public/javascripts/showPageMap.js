0471e61aa5bdabb20a7172c2e7ffe1ae4a4f56bc
ShanchaiZ <shanchai.zahid@gmail.com>
2023-02-27 20:02:32 -0500

    Completed map display on show template page. Transferred the map rendering script to public directory. Added a script that is able to read the maptoken in HTML to JS because ejs templating engine is unable to read ejs outside of html pages.

---
+++ public/javascripts/showPageMap.js
@@ -0,0 +1,9 @@
+// RENDERING A MAP SCRIPT:
+
+mapboxgl.accessToken = mapToken; //mapToken variable found as a script on showpage template
+const map = new mapboxgl.Map({
+    container: 'map', // container ID needs to match the div id in "Map Display"
+    style: 'mapbox://styles/mapbox/streets-v12', // style URL
+    center: [-74.5, 40], // starting position [longtitude, latitude]
+    zoom: 9, // starting zoom
+});
