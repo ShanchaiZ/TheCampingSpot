21153c9ad172b343341a94f5a320d2b4edd7f5d7
ShanchaiZ <shanchai.zahid@gmail.com>
2023-02-28 18:10:43 -0500

    updated seeds index.js file for seeds to display cities' longitude and latitude

--- seeds/index.js
+++ seeds/index.js
@@ -47,7 +47,10 @@ const seedDB = async () => {
             title: `${sample(descriptors)} ${sample(places)}`,
             geometry: {
                 type: "Point",
-                coordinates: [-74.5, 40]
+                coordinates: [
+                    cities[random1000].longitude,
+                    cities[random1000].latitude,
+                ]
             },
             images:
                 [
