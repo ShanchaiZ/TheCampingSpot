a51a0e77437b8b61e9612c83dfde3d872e04112f
ShanchaiZ <shanchai.zahid@gmail.com>
2023-02-28 01:35:07 -0500

    added customized popup for marker that displays title and location of marker when clicked according to mapbox docs

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -10,4 +10,10 @@ const map = new mapboxgl.Map({
 
 new mapboxgl.Marker()
     .setLngLat(campground.geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup({ offset: 25 })
+            .setHTML(
+                `<h5>${campground.title}</h5><p>${campground.location}</p>`
+            )
+    )
     .addTo(map);
